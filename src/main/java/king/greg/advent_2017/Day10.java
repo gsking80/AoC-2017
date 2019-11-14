8f86b9206d6a2fa3862d0c2fcee7f63bae3d7d03
Greg King <greg.king@gfs.com>
2019-11-14 15:39:03 -0500

    Day 10

--- src/main/java/king/greg/advent_2017/Day10.java
+++ src/main/java/king/greg/advent_2017/Day10.java
@@ -1,17 +1,67 @@
 package king.greg.advent_2017;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class Day10 {
+
+	int[] circle;
+	int[] lengths;
+	int[] denseHash = new int[16];
 	
 	public Day10(final int size) {
-		
+		circle = new int[size];
+		for (int i = 0; i < size; i++) {
+			circle[i] = i;
+		}
 	}
 	
-	public void hash(final String twists) {
-		
+	public void sparseHash(final int times) {
+		int position = 0;
+		int skipSize = 0;
+
+		for (int run = 0; run < times; run++) {
+			for (final int length : lengths) {
+//				System.out.println(length);
+				for (int j = 0; j < length / 2; j++) {
+//					System.out.println(circle[(position + j) % circle.length] + " - " + circle[(position + (length - 1) - j) % circle.length]);
+					int temp = circle[(position + j) % circle.length];
+					circle[(position + j) % circle.length] = circle[(position + (length - 1) - j) % circle.length];
+					circle[(position + (length - 1) - j) % circle.length] = temp;
+				}
+				position = (position + length + skipSize) % circle.length;
+				skipSize++;
+//				System.out.println(Arrays.toString(circle) + ", position = " + position);
+			}
+		}
 	}
 	
 	public int firstProduct() {
-		return -1;
+		return circle[0]*circle[1];
 	}
 
+	public void hashString(String input) {
+		lengths = Pattern.compile(",").splitAsStream(input).mapToInt(Integer::parseInt).toArray();
+	}
+
+	public void hashBytes(String input) {
+		final String suffixedInput = input + (char)17 + (char)31 + (char)73 + (char)47 + (char)23;
+		lengths = suffixedInput.chars().toArray();
+	}
+
+	public void denseHash() {
+		for (int i = 0; i < 16; i++) {
+			int offset = i * 16;
+			denseHash[i] = circle[offset] ^ circle[offset+1] ^ circle[offset+2] ^ circle[offset+3] ^ circle[offset+4] ^ circle[offset+5] ^ circle[offset+6] ^ circle[offset+7]
+					^ circle[offset+8] ^ circle[offset+9] ^ circle[offset+10] ^ circle[offset+11] ^ circle[offset+12] ^ circle[offset+13] ^ circle[offset+14] ^ circle[offset+15];
+		}
+	}
+
+	public String knotValue() {
+		final StringBuilder builder = new StringBuilder();
+		for(final int value: denseHash) {
+			builder.append(String.format("%02x", value));
+		}
+		return builder.toString();
+	}
 }
--- src/test/java/king/greg/advent_2017/Day10Test.java
+++ src/test/java/king/greg/advent_2017/Day10Test.java
@@ -3,13 +3,76 @@ package king.greg.advent_2017;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Day10Test {
 
 	@Test
 	public void test1a() {
 		final Day10 day10 = new Day10(5);
-		day10.hash("3, 4, 1, 5");
+		day10.hashString("3,4,1,5");
+		day10.sparseHash(1);
 		Assertions.assertThat(day10.firstProduct()).isEqualTo(12);
 	}
 
+	@Test
+	public void testSolution1() throws IOException {
+		final Day10 day10 = new Day10(256);
+		final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day10/input.txt").getPath());
+		final BufferedReader buf = new BufferedReader(fileReader);
+		day10.hashString(buf.readLine());
+		buf.close();
+		day10.sparseHash(1);
+		Assertions.assertThat(day10.firstProduct()).isEqualTo(62238);
+	}
+
+	@Test
+	public void test2a() {
+		final Day10 day10 = new Day10(256);
+		day10.hashBytes("");
+		day10.sparseHash(64);
+		day10.denseHash();
+		Assertions.assertThat(day10.knotValue()).isEqualTo("a2582a3a0e66e6e86e3812dcb672a272");
+	}
+
+	@Test
+	public void test2b() {
+		final Day10 day10 = new Day10(256);
+		day10.hashBytes("AoC 2017");
+		day10.sparseHash(64);
+		day10.denseHash();
+		Assertions.assertThat(day10.knotValue()).isEqualTo("33efeb34ea91902bb2f59c9920caa6cd");
+	}
+
+	@Test
+	public void test2c() {
+		final Day10 day10 = new Day10(256);
+		day10.hashBytes("1,2,3");
+		day10.sparseHash(64);
+		day10.denseHash();
+		Assertions.assertThat(day10.knotValue()).isEqualTo("3efbe78a8d82f29979031a4aa0b16a9d");
+	}
+
+	@Test
+	public void test2d() {
+		final Day10 day10 = new Day10(256);
+		day10.hashBytes("1,2,4");
+		day10.sparseHash(64);
+		day10.denseHash();
+		Assertions.assertThat(day10.knotValue()).isEqualTo("63960835bcdc130f0b66d7ff4f6a5a8e");
+	}
+
+	@Test
+	public void testSolution2() throws IOException {
+		final Day10 day10 = new Day10(256);
+		final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day10/input.txt").getPath());
+		final BufferedReader buf = new BufferedReader(fileReader);
+		day10.hashBytes(buf.readLine());
+		buf.close();
+		day10.sparseHash(64);
+		day10.denseHash();
+		Assertions.assertThat(day10.knotValue()).isEqualTo("2b0c9cc0449507a0db3babd57ad9e8d8");
+	}
 }
