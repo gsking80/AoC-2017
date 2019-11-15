2471ccf9da7d8b23f1c2f16d1bae027c1b357a93
Greg King <greg.king@gfs.com>
2019-11-15 17:18:32 -0500

    Day 13 part 1

---
+++ src/main/java/king/greg/advent_2017/Day13.java
@@ -0,0 +1,65 @@
+package king.greg.advent_2017;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Day13 {
+
+    final Map<Integer, Integer> scanners = new HashMap<>();
+    int maxDepth = 0;
+
+    public Day13(FileReader fileReader) {
+
+        try {
+            final BufferedReader buf = new BufferedReader(fileReader);
+
+            while(true) {
+                final String lineJustFetched = buf.readLine();
+                if(null == lineJustFetched) {
+                    break;
+                } else {
+                    process(lineJustFetched);
+                }
+            }
+        } catch (IOException ioe) {
+
+        }
+
+    }
+
+    private void process(final String input) {
+        final int[] scanner = Pattern.compile(": ").splitAsStream(input).mapToInt(Integer::parseInt).toArray();
+        if (scanner[0] > maxDepth) {
+            maxDepth = scanner[0];
+        }
+        scanners.put(scanner[0],scanner[1]);
+    }
+
+    public int severity(final int startingTime) {
+        int severity = 0;
+
+        for (int depth = 0; depth <= maxDepth; depth++) {
+            final int range = scanners.getOrDefault(depth, 0);
+            if (((depth + startingTime) % ((range - 1) * 2)) == 0) {
+                severity += (depth * range);
+            }
+        }
+
+        return severity;
+    }
+
+    public int earliestSafeStart() {
+        int delay = 0;
+
+        while (true) {
+            if (severity(delay) == 0) {
+                return delay;
+            }
+            delay++;
+        }
+    }
+}
---
+++ src/test/java/king/greg/advent_2017/Day13Test.java
@@ -0,0 +1,32 @@
+package king.greg.advent_2017;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Day13Test {
+
+    @Test
+    public void test1a() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day13/Test1.txt").getPath());
+        final Day13 day13 = new Day13(fileReader);
+        Assertions.assertThat(day13.severity(0)).isEqualTo(24);
+    }
+
+    @Test
+    public void testSolution1() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day13/input.txt").getPath());
+        final Day13 day13 = new Day13(fileReader);
+        Assertions.assertThat(day13.severity(0)).isEqualTo(1844);
+    }
+
+    @Test
+    public void test2a() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day13/Test1.txt").getPath());
+        final Day13 day13 = new Day13(fileReader);
+        Assertions.assertThat(day13.earliestSafeStart()).isEqualTo(10);
+    }
+
+}
