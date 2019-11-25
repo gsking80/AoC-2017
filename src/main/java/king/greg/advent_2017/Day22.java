3d084e117516cbb85ae48a8f6e8392dd58947f26
Greg King <greg.king@gfs.com>
2019-11-25 17:37:40 -0500

    Day 22

---
+++ src/main/java/king/greg/advent_2017/Day22.java
@@ -0,0 +1,97 @@
+package king.greg.advent_2017;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day22 {
+
+    Map<Point,Integer> grid;
+    Point node;
+    int direction;
+
+    public Day22(FileReader fileReader) {
+        try {
+            final BufferedReader buf = new BufferedReader(fileReader);
+            grid = new HashMap<>();
+
+            int y = 0;
+
+            while(true) {
+                final String lineJustFetched = buf.readLine();
+                if(null == lineJustFetched) {
+                    break;
+                } else {
+                    for (int x = 0; x < lineJustFetched.length(); x++) {
+                        grid.put(new Point(x,y), lineJustFetched.charAt(x) == '#' ? 2 : 0);
+                    }
+                    y++;
+                }
+            }
+            node = new Point(y/2, y/2);
+            direction = 0;
+        } catch (IOException ioe) {
+            throw new RuntimeException();
+        }
+    }
+
+    public int bursts(final int bursts) {
+        return bursts(bursts, false);
+    }
+
+    public int bursts(final int bursts, final boolean weaken) {
+        int infections = 0;
+
+        for (int i = 0; i < bursts; i++) {
+            int currentInfected = grid.getOrDefault(node, 0);
+            switch(currentInfected) {
+                case 0:  // clean
+                    direction = (direction + 3) % 4;  //turn left
+                    currentInfected++;
+                    if (weaken) {
+                        break;
+                    }
+                case 1:  // weakened
+                    currentInfected++;
+                    infections++;
+                    break;
+                case 2:  // infected
+                    direction = (direction + 1) % 4;  //turn right
+                    currentInfected++;
+                    if (!weaken) {
+                        currentInfected++;
+                    }
+                    break;
+                case 3:  // flagged
+                    direction = (direction + 2) % 4;  //reverse
+                    currentInfected++;
+                    break;
+            }
+
+            currentInfected %= 4;
+            grid.put(new Point(node.x, node.y), currentInfected); //swap
+
+            switch (direction) {
+                case 0:
+                    node.y -= 1;
+                    break;
+                case 1:
+                    node.x += 1;
+                    break;
+                case 2:
+                    node.y += 1;
+                    break;
+                case 3:
+                    node.x -= 1;
+                    break;
+                default:
+                    throw new RuntimeException(direction + " " + i);
+            }
+        }
+
+        return infections;
+    }
+}
---
+++ src/test/java/king/greg/advent_2017/Day22Test.java
@@ -0,0 +1,62 @@
+package king.greg.advent_2017;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import static org.junit.Assert.*;
+
+public class Day22Test {
+
+    @Test
+    public void test1a() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/Test1.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(7)).isEqualTo(5);
+    }
+
+    @Test
+    public void test1b() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/Test1.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(70)).isEqualTo(41);
+    }
+
+    @Test
+    public void test1c() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/Test1.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(10000)).isEqualTo(5587);
+    }
+
+    @Test
+    public void testSolution1() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/input.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(10000)).isEqualTo(5369);
+    }
+
+    @Test
+    public void test2a() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/Test1.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(100, true)).isEqualTo(26);
+    }
+
+    @Test
+    public void test2b() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/Test1.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(10000000, true)).isEqualTo(2511944);
+    }
+
+    @Test
+    public void testSolution2() throws FileNotFoundException {
+        final FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("day22/input.txt").getPath());
+        final Day22 day22 = new Day22(fileReader);
+        Assertions.assertThat(day22.bursts(10000000, true)).isEqualTo(2510774);
+    }
+
+}
